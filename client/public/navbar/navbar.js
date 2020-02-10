498a44d3d1052b811ca196a53033b35fa6c9a0cc
Santiago MVO <santiago.mvao@gmail.com>
2020-02-10 16:54:10 -0600

    fixed pf navbar proportions

--- client/public/navbar/navbar.js
+++ client/public/navbar/navbar.js
@@ -2,7 +2,7 @@ $("body").prepend(`
 <nav class="navbar navbar-dark fixed-top bg-dark">
     <a class="navbar-brand" href="https://evolatum.github.io/Portfolio/index.html#linkPortfolio">
         <i class="fad fa-chevron-left d-inline-block align-middle" id="backButton" alt="Go back"></i>
-        Back to portfolio
+        <span id="brand">Back to portfolio</span>
     </a>
     <span class="navbar-text" id="nameLong">
         Santiago Martinez Vara Ortiz - 10/02/20
